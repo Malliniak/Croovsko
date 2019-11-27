90cbb85212b4655192def1e4d4e35a6b163628b5
Krzysztof Maliński <malinakrzychu@gmail.com>
2019-11-27 01:31:26 +0100

    #47/levelManager and SOs

---
+++ Croovsko/Assets/_Scripts/Variables/IntReference.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class FloatReference: Reference<float>
+{
+    public FloatVariable Variable;
+    public float Value
+    {
+        get { return UseConstant ? ConstantValue : Variable.Value; }
+    }
+}
