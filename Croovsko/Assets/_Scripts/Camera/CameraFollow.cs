13b73e3cf025bebc89c7059c515c8394360e88b3
Krzysztof Maliński <malinakrzychu@gmail.com>
2019-12-08 00:39:54 +0100

    #26/review

--- Croovsko/Assets/_Scripts/Camera/CameraFollow.cs
+++ Croovsko/Assets/_Scripts/Camera/CameraFollow.cs
@@ -2,31 +2,30 @@
 
 public class CameraFollow : MonoBehaviour
 {
-    private bool _istargetNotNull;
+    private bool _isTargetNull;
 
     private Vector3 _refVelocity;
 
     [SerializeField] private float _smoothTime;
 
     public GameObject _target;
+    private Vector3 _targetPosition;
 
     // Update is called once per frame
     private void Start()
     {
-        _istargetNotNull = _target != null;
+        _isTargetNull = _target == null;
     }
 
     private void FixedUpdate()
     {
-        if (_istargetNotNull)
-        {
-            Vector3 targetPosition = new Vector3(_target.transform.position.x, _target.transform.position.y, -10f);
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _refVelocity,
-                _smoothTime * Time.deltaTime);
-        }
-        else
+        if (_isTargetNull)
         {
             Debug.LogError("Target unassigned!");
+            return;
         }
+        _targetPosition = new Vector3(_target.transform.position.x, _target.transform.position.y, -10f);
+        transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref _refVelocity, 
+            _smoothTime * Time.deltaTime);
     }
 }
