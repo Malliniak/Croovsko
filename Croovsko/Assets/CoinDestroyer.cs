c62b4d2531793046c7a82e45625fbfeb8598af10
Krzysztof Maliński <malinakrzychu@gmail.com>
2020-01-26 23:01:30 +0100

    coins fix

--- Croovsko/Assets/CoinDestroyer.cs
+++ Croovsko/Assets/CoinDestroyer.cs
@@ -1,17 +1,38 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using _Scripts.Helpers;
 using UnityEngine;
 
 public class CoinDestroyer : MonoBehaviour
 {
+
+    private bool shouldDis;
+    private LeftRightController _player;
+    public int amountToAdd = 5;
+    public IntVariable PointsRuntimeVariable;
+
+    private void Start()
+    {
+        StartCoroutine(nameof(ColliderDisable));
+        AssetLoader.GetAssetFile(out PointsRuntimeVariable, "PointsRuntime");
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        print("pong");
         if (other.GetComponent<LeftRightController>())
         {
-            print("pong");
+            _player = other.GetComponent<LeftRightController>();
+            Debug.Log("Add Points Here");
+            PointsRuntimeVariable.AddToValue(amountToAdd);
             Destroy(GetComponentInParent<CoinController>().gameObject);
+
         }
+    }
+    
+    private IEnumerator ColliderDisable()
+    {
+        yield return new WaitForSecondsRealtime(0.6f);
+        shouldDis = true;
     }
 }
